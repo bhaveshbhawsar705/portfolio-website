656411be004ee688f2c313d86378ed80d160f997
bhaveshbhawsar705 <bhaveshbhawsar705@gmail.com>
2026-01-19 18:55:14 +0530

    Add files via upload

---
+++ submit.php
@@ -0,0 +1,10 @@
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+echo "<h2>Thank You!</h2>";
+echo "Name: " . $name . "<br>";
+echo "Email: " . $email . "<br>";
+echo "Message: " . $message;
+?>
